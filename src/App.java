27d022571712b29553cd21cb1e1547d11941a9d5
Hlidskjalf <vroman197@gmail.com>
2017-10-21 11:38:27 -0400

    Change Store Number

--- src/App.java
+++ src/App.java
@@ -1,11 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class App extends JPanel implements ActionListener {
+
+    public static String storeNumber = "****";
+
     private JPanel panel1;
     private JLabel Title;
-    private JTextField StoreNumber;
+    private JButton setStore;
+
 
     // Elements declared here
 
@@ -13,13 +19,44 @@ public class App extends JPanel implements ActionListener {
 
         // Initialize and setup the elements
 
+        add(Title);
+        add(setStore);
+
+        System.out.println(Title);
+        setStore.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                storeNumber = JOptionPane.showInputDialog("Enter the store number.");
+                Title.setText("");
+                Title.setText("FCE-UTIL              Store Number: " + storeNumber);
+            }
+        });
     }
 
     protected static void build() {
 
         // Build of the JFrame, icons, and the JPane
 
+        JFrame frame = new JFrame("FCE-UTIL");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /* Custom Icon code
+        try {
+            frame.setIconImage(ImageIO.read(new File("res/cpp_logo.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } */
+
+        App pane = new App();
+        pane.setOpaque(true);
+        pane.setBackground(Color.decode("0x3B3F42"));
+        frame.setContentPane(pane);
+
+        frame.pack();
+        frame.setSize(600, 400);
+        frame.setVisible(true);
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {

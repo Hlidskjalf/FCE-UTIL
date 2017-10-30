5127bf9c187d1fbbb11ace587f459905fe8c2801
Hlidskjalf <vroman197@gmail.com>
2017-10-30 17:27:20 -0400

    ITTBuilder added

---
+++ src/ITTBuilder.java
@@ -0,0 +1,10 @@
+public class ITTBuilder extends App {
+    /**
+     * ITTBuilder class is designed to create a custom ITT file for the addition of a single
+     * item into a POS system without the need for Pricebook processing. The user will be prompted
+     * to enter all of the item specific details and a single <ITTDetail> will be created for the
+     * site.
+     */
+
+    //TODO: Break down the steps and create functions. Leverage the UpdateStores script for pushing the files
+}

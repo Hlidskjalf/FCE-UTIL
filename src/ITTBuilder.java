944169d5c98bdf271e6dba801ddd4bea0c70018e
Hlidskjalf <vroman197@gmail.com>
2017-10-31 07:07:36 -0400

    Built File IO template

--- src/ITTBuilder.java
+++ src/ITTBuilder.java
@@ -8,13 +8,18 @@ public class ITTBuilder extends App {
      * ITTBuilder class is designed to create a custom ITT file for the addition of a single
      * item into a POS system without the need for Pricebook processing. The user will be prompted
      * to enter all of the item specific details and a single <ITTDetail> will be created for the
-     * site.
+     * site. The methods createFile is generic, and is reused in SEMBuilder.java.
      */
 
     //TODO: Break down the steps and create functions. Leverage the UpdateStores script for pushing the files
 
     public static void createFile() throws IOException {
 
+        /**
+         * Method createFile handles the task of writing the ITT file to be used. The file name must be
+         * unique, so a date/time stamp is used. The actual data that is written to the file is from the
+         * buildFile method, and passed into the Writer class instance.
+         */
         String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("xml/ITT" + timeStamp + ".xml"), StandardCharsets.UTF_8))) {

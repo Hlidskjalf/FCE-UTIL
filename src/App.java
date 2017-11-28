bb46f4aa5356a6e8a250cd169db08b81a03b86c1
Hlidskjalf <vroman197@gmail.com>
2017-11-28 11:18:39 -0500

    Disabled incomplete Btns

--- src/App.java
+++ src/App.java
@@ -85,8 +85,8 @@ public class App extends JPanel implements ActionListener {
         panel.add(reboot, c);
         panel.add(termCust, c);
         panel.add(cleanUp, c);
-        panel.add(vnc, c);
-        panel.add(price_update, c);
+        //panel.add(vnc, c);
+        //panel.add(price_update, c);
         panel.add(Exit, c);
 
 
